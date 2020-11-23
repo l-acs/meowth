f358052842d99f9c077e6c275bdc2fed4de30c9d
l.acs <lucas.sahar@mail.mcgill.ca>
2020-11-22 22:12:19 -0500

    Fix POST quirk

--- src/meowth/rest.clj
+++ src/meowth/rest.clj
@@ -6,28 +6,24 @@
    [clojure.string :as str]))
 
 (defn headers [cfg]
-  {:headers
-   {"X-Auth-Token" (:token cfg),
-    "X-User-Id" (:id cfg),
-    "Content-type" "application/json"}})
+  {"X-Auth-Token" (:token cfg),
+   "X-User-Id" (:id cfg),
+   "Content-type" "application/json"})
 
 (defn rocket-gen-url [cfg call & args]
   (str (cfg :url)  "/api/v1/" call
-    (when args (->> args conj (partition 2) (map (fn [[x y]] (str x "=" y))) (str/join "&") (str "?")))))
+    (when args (->> args conj (partition 2) (map (fn [[x y]] (str x "=" y))) (str/join "&") (str "?"))))) ;; maybe don't do this
 
 (defn rocket-get [cfg call & args]
   (client/get
    (apply rocket-gen-url cfg call args)
-   (headers cfg)))
+   {:headers (headers cfg)}))
 
 (defn rocket-post [cfg call & args]
   (client/post
    (rocket-gen-url cfg call)
-   (assoc
-    (headers cfg)
-    :form-params
-    {  (first args)
-       (->> args rest (apply hash-map))})))
+   {:headers (headers cfg)
+    :form-params (apply hash-map args)}))
 
 (defn response-body
   "Gets the :body of a response as a hashmap"
