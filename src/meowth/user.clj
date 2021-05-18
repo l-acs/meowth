f8b489bf751f98bafacc541b95286dbeb2f2ba52
l-acs <lucas.sahar@mail.mcgill.ca>
2021-05-18 19:25:21 -0400

    Enrich direct-message data wrangling

--- src/meowth/user.clj
+++ src/meowth/user.clj
@@ -3,7 +3,10 @@
   (:use [meowth.rest :only [rocket-get response-body]])
   (:require
    [clojure.string :as str]
-   [clojure.set :as set]))
+   [clojure.set :as set]
+   [meowth.gather :as g]))
+
+;; this is probably mostly all bad
 
 (defn get-data-from-username [cfg username]
   (->> username (rocket-get cfg "users.info" "username") response-body :user))
@@ -11,7 +14,16 @@
 (defn get-id-from-username [cfg username]
   (->> username (get-data-from-username cfg) :_id))
 
-(defn messaged? [cfg dms id]
+(defn get-dm-info [cfg username]
+  (first
+   (filter #(= (:name %) username)
+           (g/get-user-dms cfg (:id cfg)))))
+
+(defn dm-info-fast [cfg rooms username]
+  (first
+   (filter #(= (:name %) username) rooms)))
+
+(defn messaged? [cfg dms id] ;; hm, probably make this nicer
   (some? (seq (filter #(re-matches (re-pattern (str ".*" id ".*")) %) dms))))
 
 (defn email [user]
@@ -70,3 +82,8 @@
    :messaged? (messaged? cfg (:dms cfg) (:_id user))
    :__all user ; still include _all_ info in the `user` hashmap, in case
    })
+
+
+;; note about cfg: the objective is to make a function that, when
+;; called on a namespace, will evaluate all of its functions as a
+;; second version which takes an additional `cfg` argument
