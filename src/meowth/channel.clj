d8386788a9093b67bb5aaa212ac7e6b389c2c060
l-acs <lucas.sahar@mail.mcgill.ca>
2022-07-25 22:03:10 -0400

    Provide unified interface to leader and owner management

--- src/meowth/channel.clj
+++ src/meowth/channel.clj
@@ -4,7 +4,8 @@
   (:require
    [clojure.string :as str]
    [clojure.set :as set]
-   [meowth.get :as get]))
+   [meowth.get :as get]
+   [meowth.user :refer [capitalize-first-letter]]))
 
 ;; encourage abstraction and reuse by only using full-fledged
 ;; user (constructed with gen-fields) -- not ids or usernames on
@@ -17,27 +18,27 @@
        (rocket-post :channels "addAll" :roomId)
        response-body))
 
-(defn add-leader [domain room-name user]
-  (rocket-post domain "addLeader"
-               :userId (:_id user) ;; user is a full-fledged user.clj gen-fields
-               :roomId (get/id domain room-name)))
-
-(defn remove-leader [domain room-name user]
-  (rocket-post domain "removeLeader"
-               :userId (:_id user)
-               :roomId (get/id domain room-name)))
-
-(defn add-owner [domain room-name user]
-  (rocket-post domain "addOwner"
-               :userId (:_id user)
-               :roomId (get/id domain room-name)))
-
-(defn remove-owner [domain room-name user]
-  (rocket-post domain "removeOwner"
-               :userId (:_id user)
-               :roomId (get/id domain room-name)))
-
 (defn invite-user-to-room [domain room-name user]
   (rocket-post domain "invite"
-               :userId (:_id user)
+               :userId (:id user)
                :roomId (get/id domain room-name)))
+
+(defn modify-user-in-room
+  ;; user is a full set of user fields (from gen-fields, from
+  ;; get/info), e.g.
+
+  ;; {:_id "qFG4...", :name "Luc", :rooms [...], etc. }
+
+  ;; operation is :add or :remove
+  ;; status is :leader or :owner
+
+  ;; room-name is name, not id
+  ;; domain is :channels or :groups
+  [user operation status domain room-name]
+
+  (let [operation (name operation)
+        status (capitalize-first-letter (name status))
+        verb (str operation status)]
+    (rocket-post domain verb
+                 :userId (:id user)
+                 :roomId (get/id domain room-name))))
